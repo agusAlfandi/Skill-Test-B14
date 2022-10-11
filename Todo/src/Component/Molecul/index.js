8892c46831fd8fa62d02d523f635f3597da68ace
Agus Alfandi <agusalfandi8@gmail.com>
2022-10-11 20:58:09 +0700

    Skill-Test-B14

---
+++ Todo/src/Component/Molecul/index.js
@@ -0,0 +1,5 @@
+import ButtomNavigation from './BottomNavigation';
+import DateTimePicker from './DateTimePicker';
+import Loading from './Loading';
+
+export { ButtomNavigation, DateTimePicker, Loading };
